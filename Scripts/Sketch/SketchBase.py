30956b2b3fb55288cb959222ca5041321388148f
rusted_coil <rusted.coil@gmail.com>
2022-05-17 19:27:54 +0900

    絵を描く機能試作中

--- Scripts/Sketch/SketchBase.py
+++ Scripts/Sketch/SketchBase.py
@@ -1,6 +1,18 @@
 from abc import abstractmethod
+from PIL import Image, ImageDraw
 
 class SketchBase:
+    def __init__(self, size, color):
+        self.Image = Image.new('RGBA', size, color)
+        self.Drawer = ImageDraw.Draw(self.Image)
+
     @abstractmethod
     def Draw(self):
         pass
+
+    # 半透明の四角を描画します。
+    def DrawAlphaRectangle(self, rect, color):
+        subimage = Image.new('RGBA', self.Image.size, (0, 0, 0, 0))
+        subdraw = ImageDraw.Draw(subimage)
+        subdraw.rectangle(rect, fill=color)
+        self.Image = Image.alpha_composite(self.Image, subimage)
